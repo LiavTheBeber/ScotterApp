ed23d812fe786df29fd721c614ab970727a6e75f
LiavTheBeber <liavbohadana5@gmail.com>
2023-05-02 09:47:40 +0300

    mapping im home fragment, api is missing

--- app/src/main/java/com/example/scotterapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/scotterapp/ui/home/HomeFragment.java
@@ -4,34 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.fragment.app.FragmentActivity;
 
+import com.example.scotterapp.R;
 import com.example.scotterapp.databinding.FragmentHomeBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.scotterapp.databinding.ActivityMapsBinding;
 
-public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+public class HomeFragment extends FragmentActivity implements OnMapReadyCallback {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    private ActivityMapsBinding binding;
+    private GoogleMap mMap;
+    private ActivityMapsBinding MapBinding;
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
+
+
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng ashkelon = new LatLng(31.687251, 34.582628);
+        mMap.addMarker(new MarkerOptions().position(ashkelon).title("Fire Scotter"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ashkelon));
     }
 }
